1af5e91833c39987293e057509275194d04551e1
ShubhamRoyyy <royshubham0617@gmail.com>
2026-07-15 15:54:09 +0530

    Implement welcome event

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,33 @@
+// Connect to the Socket.IO server
 const socket = io();
 
+// Get the status text
 const status = document.getElementById("status");
+
+/*
+|--------------------------------------------------------------------------
+| Connection Event
+|--------------------------------------------------------------------------
+*/
 
 socket.on("connect", () => {
 
-    console.log("Connected!");
+    console.log("✅ Connected to Server");
 
     status.innerText = "Connected";
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Welcome Event
+|--------------------------------------------------------------------------
+*/
+
+socket.on("welcome", (data) => {
+
+    console.log("🎉 Welcome Event Received");
+
+    console.log(data);
 
 });
