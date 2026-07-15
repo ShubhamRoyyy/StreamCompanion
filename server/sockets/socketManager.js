905f1cfa732a94fed72e9bb2a05b27945de90a02
ShubhamRoyyy <royshubham0617@gmail.com>
2026-07-15 16:04:33 +0530

    Refactor Socket.IO into socket manager

--- server/sockets/socketManager.js
+++ server/sockets/socketManager.js
@@ -1,14 +1,28 @@
+/**
+ * ------------------------------------------------------------
+ * Socket Manager
+ * ------------------------------------------------------------
+ * Responsible for handling every Socket.IO connection.
+ * All real-time events will live here.
+ * ------------------------------------------------------------
+ */
+
 function initializeSocket(io) {
 
     io.on("connection", (socket) => {
 
         console.log("✅ Client Connected");
 
+        // Send welcome message
         socket.emit("welcome", {
-
             title: "Stream Companion",
-
             message: "Welcome Shubham! 🎉"
+        });
+
+        // When client disconnects
+        socket.on("disconnect", () => {
+
+            console.log("❌ Client Disconnected");
 
         });
 
